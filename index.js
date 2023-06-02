ff8a45ec05df75122d290595c2836610b7b1d7a6
LucasBS4546 <125415413+LucasBS4546@users.noreply.github.com>
2023-06-01 21:24:03 -0300

    Criação das cartelas melhorada

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function gerarVetorInicial() {
     return vetor;
 }
 
-function gerarCartelaHoje() {
+function gerarNumerosCartela() {
     var vetorInicial = gerarVetorInicial();
     var cartela = [];
 
@@ -15,16 +15,24 @@ function gerarCartelaHoje() {
         cartela.push(vetorInicial[indice]);
         vetorInicial.splice(indice, 1);
     }
-    console.log(cartela);
     return cartela;
 }
 
 function criarCartela() {
+    var divCartela = document.getElementById('divCartela');
+    divCartela = document.createElement('div');
+    divCartela.id = 'divCartela';
+    var nomeCartela = document.getElementById('nomeCartela');
+    nomeCartela = document.createElement('h1');
+    nomeCartela.id = 'nomeCartela';
     var tabela = document.getElementById('tabela');
     tabela = document.createElement('table');
     tabela.id = 'tabela';
-    var section = document.getElementById('interface');
-    section.appendChild(tabela);
+    var section = document.getElementById('cartelas');
+    section.appendChild(divCartela);
+    divCartela.appendChild(nomeCartela);
+    nomeCartela.innerText = prompt("Escreva o nome do jogador!");
+    divCartela.appendChild(tabela);
     var thead = document.createElement('thead');
     tabela.appendChild(thead);
     var th1 = document.createElement('th');
@@ -43,42 +51,47 @@ function criarCartela() {
     th5.innerText = "O";
     thead.appendChild(th5);
     
-    var cartelaGerada = gerarVetorInicial();
+    var cartelaGerada = gerarNumerosCartela();
+    console.log(cartelaGerada);
 
     var linha = tabela.insertRow(-1);
-    linha.insertCell(0).innerText = 69;
-    linha.insertCell(1).innerText = 69;
-    linha.insertCell(2).innerText = 69;
-    linha.insertCell(3).innerText = 69;
-    linha.insertCell(4).innerText = 69;
+    linha.insertCell(0).innerText = cartelaGerada[0];
+    linha.insertCell(1).innerText = cartelaGerada[1];
+    linha.insertCell(2).innerText = cartelaGerada[2];
+    linha.insertCell(3).innerText = cartelaGerada[3];
+    linha.insertCell(4).innerText = cartelaGerada[4];
 
     var linha2 = tabela.insertRow(-1);
-    linha2.insertCell(0).innerText = 69;
-    linha2.insertCell(1).innerText = 69;
-    linha2.insertCell(2).innerText = 69;
-    linha2.insertCell(3).innerText = 69;
-    linha2.insertCell(4).innerText = 69;
+    linha2.insertCell(0).innerText = cartelaGerada[5];
+    linha2.insertCell(1).innerText = cartelaGerada[6];
+    linha2.insertCell(2).innerText = cartelaGerada[7];
+    linha2.insertCell(3).innerText = cartelaGerada[8];
+    linha2.insertCell(4).innerText = cartelaGerada[9];
 
     var linha3 = tabela.insertRow(-1);
-    linha3.insertCell(0).innerText = 69;
-    linha3.insertCell(1).innerText = 69;
+    linha3.insertCell(0).innerText = cartelaGerada[10];
+    linha3.insertCell(1).innerText = cartelaGerada[11];
     linha3.insertCell(2).innerText = 'X';
-    linha3.insertCell(3).innerText = 69;
-    linha3.insertCell(4).innerText = 69;
+    linha3.insertCell(3).innerText = cartelaGerada[12];
+    linha3.insertCell(4).innerText = cartelaGerada[13];
 
     var linha4 = tabela.insertRow(-1);
-    linha4.insertCell(0).innerText = 69;
-    linha4.insertCell(1).innerText = 69;
-    linha4.insertCell(2).innerText = 69;
-    linha4.insertCell(3).innerText = 69;
-    linha4.insertCell(4).innerText = 69;
+    linha4.insertCell(0).innerText = cartelaGerada[14];
+    linha4.insertCell(1).innerText = cartelaGerada[15];
+    linha4.insertCell(2).innerText = cartelaGerada[16];
+    linha4.insertCell(3).innerText = cartelaGerada[17];
+    linha4.insertCell(4).innerText = cartelaGerada[18];
 
     var linha5 = tabela.insertRow(-1);
-    linha5.insertCell(0).innerText = 69;
-    linha5.insertCell(1).innerText = 69;
-    linha5.insertCell(2).innerText = 69;
-    linha5.insertCell(3).innerText = 69;
-    linha5.insertCell(4).innerText = 69;
+    linha5.insertCell(0).innerText = cartelaGerada[19];
+    linha5.insertCell(1).innerText = cartelaGerada[20];
+    linha5.insertCell(2).innerText = cartelaGerada[21];
+    linha5.insertCell(3).innerText = cartelaGerada[22];
+    linha5.insertCell(4).innerText = cartelaGerada[23];
 
 
+}
+
+function reiniciarJogo() {
+    location.reload();
 }
